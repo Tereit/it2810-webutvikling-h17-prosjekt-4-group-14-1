58f4c2c6fc052105d187de1e800ac0896e8ecd50
henrikbossart <henrikbossart@me.com>
2017-10-26 17:27:43 +0200

    created a main.ts file. Also added the navbar to the app.component.html file

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app/app.component.html'
+})
+export class AppComponent {
+  title = 'Welcome';
+  message = 'This page is still under construction. Check back in November 2017.'
+}
