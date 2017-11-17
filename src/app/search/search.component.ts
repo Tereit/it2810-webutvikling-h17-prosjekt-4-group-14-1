4933ff3da31b8a9d53168ad7336b326c3d5cb5e9
Tereit <morten_mulvik@hotmail.com>
2017-11-17 12:38:56 +0100

    search will now actually check the database and display the results if there are any

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ArtistService } from '../services/artist.service';
+import { Artist } from '../models/artist.model';
 
 @Component({
     selector: 'app-search',
@@ -8,16 +9,14 @@ import { ArtistService } from '../services/artist.service';
 })
 
 export class SearchComponent implements OnInit {
+    column: any = 5;
     value = '';
-    artistSearchResult: Object = [];
+    artistSearchResult: Artist[] = [];
     constructor(private artistService: ArtistService) { }
 
     getArtist(): void {
-        this.artistSearchResult = this.artistService.getArtist(this.value);
-    }
-
-    getAllArtists(): void {
-        this.artistSearchResult = this.artistService.getAllArtists();
+        console.log('search para: ' + this.value);
+        this.artistService.getArtist(this.value).subscribe(data => this.artistSearchResult = data);
     }
 
     onKey(event: any) {
