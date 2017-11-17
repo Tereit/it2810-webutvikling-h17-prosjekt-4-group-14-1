f1b4cef448e4c4a78f3c31811a9a7701dcfe8c20
Tereit <morten_mulvik@hotmail.com>
2017-11-17 10:25:38 +0100

    added/modified files that got lost in merge

--- src/api/routes/api.route.js
+++ src/api/routes/api.route.js
@@ -2,7 +2,9 @@ var express = require('express');
 
 var router = express.Router();
 var artist = require('./api/artist.route');
+var lfm    = require('./api/lfm.route');
 
 router.use('/artist', artist);
+router.use('/lfm', lfm);
 
 module.exports = router;
