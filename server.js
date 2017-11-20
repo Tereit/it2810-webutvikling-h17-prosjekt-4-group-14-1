3fcca8afae418cbbc9ddfc2f9adfa8c6a4eaa334
henrikbossart <henrikbossart@me.com>
2017-11-20 14:20:55 +0100

    added support for CORS

--- server.js
+++ server.js
@@ -24,6 +24,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 */
 
+// Allowing CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api', api);
 
 app.use("/", express.static(__dirname + '/'));
